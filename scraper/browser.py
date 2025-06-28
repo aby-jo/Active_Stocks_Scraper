264d283eaa56c764f6402c722cb09bfd60ad77e8
Aby Joseph <173944472+aby-jo@users.noreply.github.com.>
2025-06-29 04:10:47 +0530

    Updated browser..py to remove conflict when running in Github actions

--- scraper/browser.py
+++ scraper/browser.py
@@ -1,10 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+import tempfile
 def create_browser(headless=True):
     options = webdriver.ChromeOptions()
+    # Headless browser setting
     if headless:
-        options.add_argument("--headless")
+        options.add_argument("--headless=new")  # Use the modern headless mode
+    
+    # Prevent profile conflict
+    user_data_dir = tempfile.mkdtemp()  # Create a temp profile for Chrome
+    options.add_argument(f"--user-data-dir={user_data_dir}")
+
+    # Other common flags to improve compatibility and performance
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
