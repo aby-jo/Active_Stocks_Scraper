43a7874607f6ebb0ae6602c89ef8dbb46b5c2b73
Aby Joseph <173944472+aby-jo@users.noreply.github.com.>
2025-06-29 04:23:51 +0530

    Update waits in webdriver to be longer

--- scraper/navigation.py
+++ scraper/navigation.py
@@ -4,12 +4,12 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def go_to_next_page(driver, prev_first_row):
     try:
-        next_btn = WebDriverWait(driver, 10).until(
+        next_btn = WebDriverWait(driver, 30).until(
             EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='next-page-button']"))
         )
         next_btn.click()
         # Wait until the previously passed row element becomes stale (i.e., the DOM changes)
-        WebDriverWait(driver,10).until(lambda d : d.find_element(By.CSS_SELECTOR,"tbody tr").text!=prev_first_row)
+        WebDriverWait(driver,30).until(lambda d : d.find_element(By.CSS_SELECTOR,"tbody tr").text!=prev_first_row)
         return True
     except:
         return False
